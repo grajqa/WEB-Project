516f5c989ab791556b41b3b89e1429f29849682e
= <150240896+Rinarda@users.noreply.github.com>
2024-03-21 15:42:45 +0100

    Pjesa e footerit me php

---
+++ footeri.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footeri.css">
+</head>
+<body>
+<footer class="footeri">
+        <div class="footeritems">
+            <h2>About Sephora</h2>
+            <ul>
+            <li><a href="#">About Sephora</a></li>
+            <li><a href="#">Newsroom</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Sephora Values</a></li>
+            <li><a href="#">Supply Chain Transparency</a></li>
+            <li><a href="#">Affiliates</a></li>
+            <li><a href="#">Sephora Events</a></li>
+            <li><a href="#">Gift Cards</a></li>
+            <li><a href="#">Sephora Global Sites</a></li>
+            <li><a href="#">Diversity, Equity & Inclusion</a></li>
+            <li><a href="#">Sephora Accelerate</a></li>
+            <li><a href="#">Beauty (Re)Purposed</a></li>
+            <li><a href="#">Report a Vulnerability</a></li>
+            </ul>
+        </div>
+        <div class="footeritems">
+            <h2>My Sephora</h2>
+            <ul>
+            <li><a href="#">Beauty Insider</a></li>
+            <li><a href="#">Sephora Credit Card</a></li>
+            <li><a href="#">Community Profile</a></li>
+            <li><a href="#">Order Status</a></li>
+            <li><a href="#">Purchase History</a></li>
+            <li><a href="#">Account Settings</a></li>
+            <li><a href="#">Beauty Advisor Recommendations</a></li>
+            <li><a href="#">Beauty Offers</a></li>
+            <li><a href="#">Quizzes & Buying Guides</a></li>
+            <li><a href="#">Dewards Bazaar</a></li>
+            <li><a href="#">Loves</a></li>
+            <li><a href="#">Book a Reservation</a></li>
+            </ul>
+        </div>
+        <div class="footeritems">
+            <h2>Help</h2>
+            <ul>
+            <li><a href="#">Customer Service</a></li>
+            <li><a href="#">Returns & Exchanges</a></li>
+            <li><a href="#">Delivery and Pickup Options</a></li>
+            <li><a href="#">Shipping</a></li>
+            <li><a href="#">Billing</a></li>
+            <li><a href="#">International Shipments</a></li>
+            <li><a href="#">Beauty Services FAQ</a></li>
+            <li><a href="#">Sephora at Kohl's</a></li>
+            <li><a href="#">Store Locations</a></li>
+            <li><a href="#">Online Ordering</a></li>
+            <li><a href="#">Klarna & Afterpay</a></li>
+            <li><a href="#">Accessibility</a></li>
+            </ul>
+        </div>
+        <div class="footeritems">
+            <h2>Our Socials</h2>
+            <div class="socials">
+                <a href="https://www.instagram.com/sephora/?hl=en">
+                <img src="instagram-ko.svg" style="max-width: 60px;" />
+                </a>
+                <a href="https://www.tiktok.com/@sephora?lang=en">
+                <img src="tiktok-ko.svg" style="max-width: 60px;" />
+                </a>
+                <a href="https://www.pinterest.com.au/sephora/">
+                    <img src="pinterest-ko.svg" style="max-width: 60px;" />
+                </a>
+                <a href="https://twitter.com/sephora?lang=en">
+                    <img src="twitter-ko.svg" style="max-width: 60px;" />
+                </a>
+                <a href="https://www.youtube.com/user/sephora/tutorials">
+                    <img src="youtube-ko.svg" style="max-width: 60px;" />
+                </a>
+        </div>
+    </footer>
+    
+</body>
+</html>
