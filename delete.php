5c5dcde735204bf108f37132063d6fe5fda19bd3
= <150240896+Rinarda@users.noreply.github.com>
2024-03-24 19:05:11 +0100

    delete.php

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+require_once('dbcon.php');
+
+if (!isset($_GET['id'])) {
+    echo 'error';
+    exit;
+}
+$userId = $_GET['id'];
+
+$sql = "DELETE FROM products WHERE id = ?";
+$stmt = $db->prepare($sql);
+
+$result = $stmt->execute([$userId]);
+
+if ($result) {
+    header("Location: dashProducts.php");
+    exit;
+} else {
+    echo "Error deleting product";
+}
+?>
+
